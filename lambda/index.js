5fc852348ba418cfc1bdd6c06a3b74f57f5ca079
MayHyeyeonKim <hkim7963@gmail.com>
2024-04-11 16:11:14 -0700

    Lambda and DynaoDB connection successful

--- lambda/index.js
+++ lambda/index.js
@@ -6,42 +6,19 @@ const region = "us-east-2";
 const s3 = new S3Client({ region });
 const mydynamoDB = DynamoDBDocument.from(new DynamoDB({}));
 const { nanoid } = require("nanoid");
-
-// Set your AWS resources
+const no = nanoid();
 require("dotenv").config();
 const myBucket = process.env.MY_BUCKET;
 const tableName = process.env.TABLE_NAME;
-const contentT = "text/plain";
-
-
-// let nanoid;
-async function generateNanoid() {
-  if (typeof nanoid === "undefined") {
-    const { nanoid: localNanoid } = await import("nanoid");
-    nanoid = localNanoid;
-  }
-  return nanoid;
-}
 
 exports.handler = async (event) => {
-  const responseBody = {
-    message: "이제 잘 되지?",
-  };
-
-  let body = JSON.stringify(responseBody);
-  console.log("body는: ", body)
   const headers = {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": true,
   };
 
-  const no = await generateNanoid();
-  console.log("no는: ", no);
-  
   let inputData;
- 
-  
   try {
     inputData = JSON.parse(event.body);
   } catch (e) {
@@ -50,47 +27,38 @@ exports.handler = async (event) => {
       statusCode: 400,
     };
   }
+
   const { inputText, fileName } = inputData;
   const bucketName = myBucket;
   const fileKey = `${fileName}`;
-  // const fileKey = `${fileName}.txt`;
- console.log("inputData는:", inputData, 
- "  bucketName은: ", myBucket, 
- "  fileKey는: ", fileKey);
+  const dbParams = {
+    TableName: tableName,
+    Item: {
+      no: no,
+      inputText: inputText,
+      inputFile: `s3://${bucketName}/${fileKey}`,
+    },
+  };
 
- const dbParams = {
-   TableName: tableName,
-   Item: {
-     id: no,
-     inputText: inputText,
-     inputFile: `s3://${bucketName}/${fileKey}`,
-   },
- };
-  // return {
-  //   statusCode: 200,
-  //   headers,
-  //   body: "",
-  // };
   try {
     const ourURL = await getSignedUrl(
       s3,
       new PutObjectCommand({
         Bucket: bucketName,
         Key: fileKey,
-      ContentType: 'image/png',
+        ContentType: "image/png",
       }),
       { expiresIn: 3600 }
     );
-    // console.log("ourURL은: ", ourURL);
-    // await mydynamoDB.put(dbParams);
-    // console.log("dbParams는: ", dbParams);
+    await mydynamoDB.put(dbParams);
 
     return {
       statusCode: 200,
       headers,
       body: JSON.stringify({
-        // id: dbParams.Item.id,
-        // s3Path: dbParams.Item.inputFile,
+        id: no,
+        inputText: inputText,
+        inputFile: `s3://${bucketName}/${fileKey}`,
         ourURL,
       }),
     };
@@ -99,8 +67,7 @@ exports.handler = async (event) => {
     // console.error('Error:', err);
     return {
       statusCode: 500,
-      headers,
-      body,
+      headers
     };
   }
 };
